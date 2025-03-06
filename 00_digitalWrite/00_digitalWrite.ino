82baca03ceb1954799f6b83370a7a8046fdee74f
svenzer007 <42415753+Nidhushan@users.noreply.github.com>
2025-03-06 12:24:36 -0500

    digitalWrite
    Made the led blink in every 0.5 seconds

--- 00_digitalWrite/00_digitalWrite.ino
+++ 00_digitalWrite/00_digitalWrite.ino
@@ -1,9 +1,21 @@
+int ledState;
+long lastChange;
+
 void setup() {
   // TODO: declare output pin
-
+  pinMode(3, OUTPUT);
+  ledState = LOW;
+  // ledState = HIGH;
+  lastChange = millis();
 }
 
 void loop() {
   // TODO: change output value every X seconds
+  digitalWrite(3, ledState);
 
+  if (millis() - lastChange > 500) {
+    ledState = !ledState;
+    lastChange = millis();
+  }
 }
+
