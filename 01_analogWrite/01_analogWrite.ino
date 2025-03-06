a88c7ac2a08d7d68b8f9803d71feda271f5f7543
svenzer007 <42415753+Nidhushan@users.noreply.github.com>
2025-03-06 12:52:42 -0500

    analogWrite
    Made the led fade and sort of increase the intensity of light in the led.

--- 01_analogWrite/01_analogWrite.ino
+++ 01_analogWrite/01_analogWrite.ino
@@ -1,9 +1,26 @@
+int ledState;
+long lastChange;
+
 void setup() {
   // TODO: declare output pin
+  pinMode(3, OUTPUT);
 
+  ledState = 0;
+  // ledState = 1;
+  lastChange = millis();
 }
 
 void loop() {
-  // TODO: update output value, check for restart
+  // TODO: change output value every X seconds
+  analogWrite(3, ledState);
 
+  if (millis() - lastChange > 10) {
+    ledState += 1;
+    lastChange = millis();
+    // USE IF NEEDED - IT WORKED WITHOUT THIS PART WHEN I RAN IT.
+    // if (ledState > 255) {
+    //   ledState = 0;
+    // }
+  }
 }
+
