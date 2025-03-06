60b8101915e7de34339345ae189775e7c611428f
svenzer007 <42415753+Nidhushan@users.noreply.github.com>
2025-03-06 13:43:55 -0500

    Servo Motor
    Made the servo motor go back and forth, in different timings and ways.

--- 02_analogRead/02_analogRead.ino
+++ 02_analogRead/02_analogRead.ino
@@ -1,9 +1,10 @@
 void setup() {
   // TODO: create serial object
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // TODO: read analog and output to serial
-
+  int a1 = analogRead(A1);
+  Serial.println(a1);
 }
--- 05_servoMotor/05_servoMotor.ino
+++ 05_servoMotor/05_servoMotor.ino
@@ -1,16 +1,28 @@
 #include <Servo.h>
 
 int inPin = A0;
-
+int servoAngle = 0;
+int servoVelocity = 2;
+int lastChange = 0;
 Servo mServo;
 
 void setup() {
   Serial.begin(57600);
-  mServo.attach(3);
+  mServo.attach(inPin);
 }
 
 void loop() {
-  int a0Val = analogRead(inPin);
+  // int a0Val = analogRead(inPin);
+  // analogWrite(A0, servoAngle);
 
-  // TODO: use a0Val on servo output
+  if (millis() - lastChange > 15) {
+    servoAngle += servoVelocity;
+    lastChange = millis();
+    if (servoAngle > 180 || servoAngle<0) {
+      servoVelocity = -servoVelocity;
+    }
+  }
+
+  // write the angle to the motor
+  mServo.write(servoAngle);
 }
